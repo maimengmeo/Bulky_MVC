5de8d8921d9e03484e1eb8325909ac45772b16e7
Mai Pham <mai.pham@naryant.com>
2023-10-16 20:02:33 -0400

    comment added to explain

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,6 +1,11 @@
+//Program.cs and startup.cs is combined in new version
+//whenever have to configure something in the pipeline, go to this file
+//pipeline means when a req comes to an app, how do you want to process that
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//this means we're using conrtoller
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -14,14 +19,15 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles();//call files in wwwroot. all the files in there will be accessible in the app
 
-app.UseRouting();
+app.UseRouting();//add routing to the req pipeline
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
+app.MapControllerRoute(// tell the app how the routing should work
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");//the ? in .net means can be defined or not
+    //for default, nav to home/index, id is optional
 
 app.Run();
